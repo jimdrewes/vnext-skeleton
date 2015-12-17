8003fe42c952840f29e70e9446d963aa93fe88e5
Jim Drewes <jim@jimdrewes.com>
2015-12-16 21:46:10 -0600

    Initial Import.

---
+++ src/JD.TreadHud.Api/Controllers/ActivitiesController.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNet.Mvc;
+using JD.TreadHud.Domain.Models;
+
+namespace JD.TreadHud.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class ActivitiesController : Controller
+    {
+        // GET: api/activity
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/activity/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/activity
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/activity/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/activity/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ src/JD.TreadHud.Domain/Models/Activity.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JD.TreadHud.Domain.Models
+{
+    public class Activity
+    {
+        public Guid Id;
+        public DateTime StartDate;
+        public DateTime? EndDate;
+    }
+}
